0bcdd9e698a9624405da3b00e157dbc849353af2
pola0104 <pola0111.0444@gmail.com>
2026-06-24 17:22:35 +0800

    Add JuniorGardener route to App router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes} from "react-router-dom"
 import Home from "./pages/Home"
+import JuniorGardener from "./pages/JuniorGardener";
 import "./styles/app.css"
 function App() {
   return(
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/junior-gardener" element={<JuniorGardener />} />
     </Routes>
   );
 }
