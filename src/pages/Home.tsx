aab10df2d01e0b7a336b71a2bd67a8eae4809a03
pola0104 <pola0111.0444@gmail.com>
2026-06-24 12:53:29 +0800

    added it so that the file is sent to the server

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -23,6 +23,16 @@ function Home() {
                 return;
             }
             setFileName(selectedFile.name);
+            const formData = new FormData();
+            formData.append("file", selectedFile);
+
+            fetch("http://localhost:8000/load-file", {
+            method: "POST",
+            body: formData
+            })
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.error("Fetch error:", err));
         }
     }
     return (
