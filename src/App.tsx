a39cf882b29b36ee283bb4047c6f7ded1178320a
pola0104 <pola0111.0444@gmail.com>
2026-06-27 13:08:52 +0800

    The route has been removed and now it is the settings of the modal window, not a separate page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { Route, Routes} from "react-router-dom"
 import Home from "./pages/Home"
 import JuniorGardener from "./pages/JuniorGardener";
 import "./styles/app.css"
-import SettingsGardener from "./components/SettingsGardener";
 function App() {
   return(
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/junior-gardener" element={<JuniorGardener />} />
-      <Route path="/settings-gardener" element={<SettingsGardener />}/>
     </Routes>
   );
 }
