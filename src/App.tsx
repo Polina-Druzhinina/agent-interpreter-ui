c6e40994c4e5a03971c78fd8b14bc332364020e0
pola0104 <pola0111.0444@gmail.com>
2026-07-01 15:07:47 +0800

    Removed AppContext, use useGardener hook, pass gardenerProps to routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,55 +3,18 @@ import Home from "./pages/Home";
 import JuniorGardener from "./pages/JuniorGardener";
 import "./styles/app.css";
 import GardenerEditMode from "./pages/GardenerEditMode";
-import { createContext, useState, useEffect, Dispatch, SetStateAction } from "react";
 import LaunchGardener from "./pages/LaunchGardener";
+import useGardener from "./hooks/useGardener";
 
-export const AppContext = createContext({
-	width: 10,
-	setWidth: (() => {}) as Dispatch<SetStateAction<number>>,
-	height: 8,
-	setHeight: (() => {}) as Dispatch<SetStateAction<number>>,
-	orientation: "south",
-	setOrientation: (() => {}) as Dispatch<SetStateAction<string>>,
-	matrix: [] as string[][],
-	setMatrix: (() => {}) as Dispatch<SetStateAction<string[][]>>,
-});
 function App() {
-	const [width, setWidth] = useState(10);
-	const [height, setHeight] = useState(8);
-	const [orientation, setOrientation] = useState("south");
-	const [matrix, setMatrix] = useState(() =>
-		Array.from({ length: 8 }, () => Array(10).fill("emptiness")),
-	);
-
-	useEffect(() => {
-		setMatrix((prev) =>
-			Array.from({ length: height }, (_, h) =>
-				Array.from({ length: width }, (_, w) => prev[h]?.[w] ?? "emptiness"),
-			),
-		);
-	}, [width, height]);
-
+	const gardenerProps = useGardener();
 	return (
-		<AppContext.Provider
-			value={{
-				width,
-				setWidth,
-				height,
-				setHeight,
-				orientation,
-				setOrientation,
-				matrix,
-				setMatrix,
-			}}
-		>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/junior-gardener" element={<JuniorGardener />} />
-				<Route path="/preview" element={<GardenerEditMode />} />
-				<Route path="/launch" element={<LaunchGardener />} />
-			</Routes>
-		</AppContext.Provider>
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/junior-gardener" element={<JuniorGardener {...gardenerProps}/>} />
+			<Route path="/preview" element={<GardenerEditMode {...gardenerProps}/>} />
+			<Route path="/launch" element={<LaunchGardener {...gardenerProps}/>} />
+		</Routes>
 	);
 }
 
